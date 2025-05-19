88745db0c23a8ca175831f0bfc76e84d394eff86
Raul Cardoso <byruzie@gmail.com>
2025-05-18 22:27:55 -0300

    ending drag function

--- components/Kanban.jsx
+++ components/Kanban.jsx
@@ -1,9 +1,9 @@
 import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 import { useState } from "react";
 
-export const KanbanCard = ({ cards }) => {
+export const KanbanCard = ({ card }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: "draggable",
+    id: card.id.toString(),
   });
 
   const style = transform
@@ -13,50 +13,46 @@ export const KanbanCard = ({ cards }) => {
     : undefined;
 
   return (
-    <div className="flex flex-col gap-2">
-      {cards.map((card) => (
-        <div
-          className={`flex flex-col gap-2 border border-slate-200 rounded-lg bg-slate-50 min-h-20 w-full`}
-          key={card.id}
-          style={style}
-          {...listeners}
-          {...attributes}
-          ref={setNodeRef}
-        >
-          <div className="flex flex-col gap-2 p-2">
-            <div>
-              <p className="font-semibold text-sm text-slate-800">
-                {card.name}
-              </p>
-            </div>
-            <div>
-              <p className="text-xs text-slate-600">{card.description}</p>
-              <p className="text-xs text-slate-600">
-                {card.startAt} - {card.endAt}
-              </p>
-            </div>
-          </div>
+    <div
+      className={`flex flex-col gap-2 border border-slate-200 rounded-lg bg-slate-50 min-h-20 w-full`}
+      key={card.id}
+      style={style}
+      {...listeners}
+      {...attributes}
+      ref={setNodeRef}
+    >
+      <div className="flex flex-col gap-2 p-2">
+        <div>
+          <p className="font-semibold text-sm text-slate-800">{card.name}</p>
         </div>
-      ))}
+        <div>
+          <p className="text-xs text-slate-600">{card.description}</p>
+          <p className="text-xs text-slate-600">
+            {card.startAt} - {card.endAt}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export const KanbanBoard = ({ statusList, cards }) => {
-  const { isOver, setNodeRef } = useDroppable({ id: "droppable" });
-
   return (
     <div className="flex gap-2">
       {statusList.map((status) => {
+        const { setNodeRef } = useDroppable({
+          id: status.id.toString(),
+        });
+
         const filteredCards = cards.filter(
           (card) => card.status.id === status.id
         );
 
         return (
           <div
-            className={`w-50 min-h-54 border border-slate-200 rounded-lg bg-slate-100 flex gap-2 justify-center p-2 pt-2 
-              ${isOver ? "border-b-emerald-600" : "border-amber-400"} 
-              `}
+            className={
+              "w-50 min-h-76 max-h-76 border border-slate-200 rounded-lg bg-slate-100 flex gap-2 justify-center p-2 pt-2"
+            }
             key={status.id}
             ref={setNodeRef}
           >
@@ -72,8 +68,10 @@ export const KanbanBoard = ({ statusList, cards }) => {
                   </p>
                 </div>
               </div>
-              <div>
-                <KanbanCard cards={filteredCards} />
+              <div className="flex flex-col gap-2">
+                {filteredCards.map((card) => (
+                  <KanbanCard key={card.id} card={card} />
+                ))}
               </div>
             </div>
           </div>
@@ -83,13 +81,27 @@ export const KanbanBoard = ({ statusList, cards }) => {
   );
 };
 
-export const KanbanProvider = ({ statusList, cards }) => {
-  const [isDropped, setIsDropped] = useState(false);
+export const KanbanProvider = ({ statusList, cards: initialCards }) => {
+  const [cards, setCards] = useState(initialCards);
 
   const handleDragEnd = (event) => {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
-    }
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const cardId = parseInt(active.id);
+    const newStatusId = over.id;
+
+    setCards((prevCards) =>
+      prevCards.map((card) =>
+        card.id === cardId
+          ? {
+              ...card,
+              status: statusList.find((status) => status.id === newStatusId),
+            }
+          : card
+      )
+    );
   };
 
   return (
